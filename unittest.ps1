5e709913f1da1ca98dc9fcc3ac864dea0e18dc5c
Konstantin Nosov <konstantin.nosov@gmail.com>
2025-01-25 23:58:19 +0200

    tests structure

--- unittest.ps1
+++ unittest.ps1
@@ -2,4 +2,27 @@
 # so mounting it to /src and copying to /apps and run helm unittest there for it to work with linux filesystem
 # better solution would be to install helm-unitest on windows but it fails 
 # docker run -ti --rm -v .:/src --entrypoint /bin/sh helmunittest/helm-unittest:3.16.3-0.7.0 -c "cp -R /src/* /apps && cd /apps && helm unittest . $args && rm -rf /src/tests/__snapshot__ && cp -R /apps/tests/__snapshot__ /src/tests/" > unittests.txt
-docker run -ti --rm -v .:/src --entrypoint /bin/sh helmunittest/helm-unittest:3.16.3-0.7.0 -c "cp -R /src/* /apps && cd /apps && helm unittest . $args && rm -rf /src/tests/__snapshot__ && cp -R /apps/tests/__snapshot__ /src/tests/" 
+
+
+# Define the default -f argument
+$defaultArg = "-f 'tests/**/*_test.yaml'"
+
+# Parse the provided arguments
+$providedArgs = @() # Initialize an array for processed arguments
+$hasFileArg = $false # Flag to check if -f is present
+
+foreach ($arg in $args) {
+  if ($arg -match "^-f$") {
+      # If -f is detected, mark the flag as true
+      $hasFileArg = $true
+  }
+  $providedArgs += $arg
+}
+
+# Add the default -f argument if none is found
+if (-not $hasFileArg) {
+  $providedArgs += $defaultArg
+}
+
+
+docker run -ti --rm -v .:/src --entrypoint /bin/sh helmunittest/helm-unittest:3.16.3-0.7.0 -c "cp -R /src/* /apps && cd /apps && helm unittest . $providedArgs && rm -rf /src/tests/__snapshot__ && cp -R /apps/tests/__snapshot__ /src/tests/" 
